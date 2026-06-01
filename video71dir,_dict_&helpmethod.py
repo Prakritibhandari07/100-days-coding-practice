8ffcf3c8be083e0f0f7b2ca86cb4dc510a72f767
Prakriti Bhandari <prakritibhandari555@gmail.com>
2026-06-01 07:19:18 +0545

    Format code and preserve functionality in helpmethod.py

--- video71dir,_dict_&helpmethod.py
+++ video71dir,_dict_&helpmethod.py
@@ -13,8 +13,10 @@ p=Person("Prakriti",18)
 print(p.__dict__)        
 
 #dict=helps to change any form into dictionary
+
 print(help(str))
 print(help(Person))
+
 #The help function is used to used to get help documentation
 #for an object,including a description of it's attributes
 #and methods.
